02b56ffff33b57c3a0316b6ab4a06dd97c6f587f
Dmytri Kleiner <dev@dmytri.to>
2024-12-30 14:49:19 +0100

    ...

--- tests/deploy.feature
+++ tests/deploy.feature
@@ -14,9 +14,18 @@ Feature: Charmebracelet Soft Serve Git Host
 
   @dev @ci @prod
   Scenario: Verify expected host OS
+    Given apk packages must be latest
     Then OS is Alpine Linux 3.21 
 
   @dev @ci @prod
+  Scenario: Verify Soft Serve 0.8.1 Checksums
+    When cosign is required
+     And Soft Serve checksums file is required
+     # And Checksum file must be verified with cosign
+
+  @dev @ci @prod
   Scenario: Require Soft Serve
-    When host must run Soft Serve
+    # Given I have a Soft Serve Package
+    #   And the checksum matches
+    When Soft Serve is required
     Then Soft Serve is available
--- tests/test_deploy.py
+++ tests/test_deploy.py
@@ -1,5 +1,5 @@
 from pytest import fixture
-from pytest_bdd import scenarios, given, when, then
+from pytest_bdd import scenarios, scenario, given, when, then
 from pyinfra.api.host import Host
 from pyinfra.api.inventory import Inventory
 from pyinfra.api.operation import add_op
@@ -10,13 +10,13 @@ from pyinfra.api.config import Config
 from pyinfra.facts.server import LinuxDistribution, LinuxDistributionDict, Arch
 from pyinfra.facts.apk import ApkPackages
 from pyinfra.facts.openrc import OpenrcStatus, OpenrcEnabled
-from pyinfra.operations import files, server, openrc
+from pyinfra.operations import files, server, apk, openrc
 
 from typing import Literal
 from io import StringIO
 from textwrap import dedent
 
-scenarios("./deploy.feature")
+scenarios("deploy.feature")
 
 Targets = Literal["ci", "dev", "prod"] 
 TARGET: Targets | None = None
@@ -70,17 +70,24 @@ def state() -> State:
 
     return state
 
+scenario("deploy.feature", "dev")
+
 @when("target is dev")
 def _():
     global TARGET
     assert TARGET is None
     TARGET = "dev"
 
+scenario("deploy.feature", "ci")
+
 @when("target is ci")
 def _():
     global TARGET
     assert TARGET is None
+
     TARGET = "ci"
+
+scenario("deploy.feature", "prod")
 
 @when("target is prod")
 def _():
@@ -88,12 +95,47 @@ def _():
     assert TARGET is None
     TARGET = "prod"
 
+scenario("deploy.feature", "Verify expected host OS")
+
+@given("apk packages must be latest")
+def _(state):
+    add_op(state,
+       apk.update
+    )
+    add_op(state,
+       apk.upgrade
+    )
+    run_ops(state)
+
 @then("OS is Alpine Linux 3.21")
 def _(host: Host):
     distro: LinuxDistributionDict = host.get_fact(LinuxDistribution)
     assert distro["release_meta"]["PRETTY_NAME"] == "Alpine Linux v3.21"
 
-@when("host must run Soft Serve")
+scenario("deploy.feature", "Verify Soft Serve 0.8.1 Checksums")
+
+@when("cosign is required")
+def _(state: State):
+    add_op(state,
+       apk.packages,
+       packages = ["cosign"]
+    )
+
+@when("Soft Serve checksums file is required")
+def _(state: State):
+    version: str = "0.8.1"
+    add_op(state,
+        files.download,
+        name="Download Soft Serve Checksums",
+        src=f"https://github.com/charmbracelet/soft-serve/releases/download/v{version}/checksums.txt",
+        dest="/root/checksums.txt"
+    )
+
+    run_ops(state)
+
+scenario("deploy.feature", "Require Soft Serve")
+
+@when("Soft Serve is required")
 def _(state: State, host: Host):
     version: str = "0.8.1"
     packages: dict = host.get_fact(ApkPackages)
